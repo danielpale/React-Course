2e1d52b6703ee872afab7f6ec7b92fa73749c767
danielpale <danielpp79@gmail.com>
2024-08-13 15:00:57 -0600

    feat: Food app (project 18) started

---
+++ Section 18 - Food App/src/components/Cart.jsx
@@ -0,0 +1,49 @@
+import { useContext } from "react";
+
+import Modal from "./UI/Modal";
+import Button from "./UI/Button";
+
+import CartContext from "../store/CartContext";
+import UserProgressContext from "../store/UserProgressContext";
+
+import { currencyFormatter } from "../util/formatting";
+import CartItem from "./CartItem";
+
+export default function Cart() {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const cartTotal = cartCtx.items.reduce(
+    (accum, curr) => accum + curr.quantity * curr.price,
+    0
+  );
+
+  function handleCloseCart() {
+    userProgressCtx.hideCart();
+  }
+
+  return (
+    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
+      <h2>Your Cart</h2>
+      <ul>
+        {cartCtx.items.map((item) => (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            price={item.price}
+            quantity={item.quantity}
+            onIncrease={() => cartCtx.addItem(item)}
+            onDecrease={() => cartCtx.removeItem(item.id)}
+          />
+        ))}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
+        {cartCtx.items.length > 0 && <Button>Go to Checkout</Button>}
+      </p>
+    </Modal>
+  );
+}
