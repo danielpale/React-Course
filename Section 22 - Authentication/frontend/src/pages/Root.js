39dc11800fab38585e32b5265fcd4c2940595822
danielpale <danielpp79@gmail.com>
2025-05-23 14:58:55 -0600

    feat: Authentication project finished

---
+++ Section 22 - Authentication/frontend/src/pages/Root.js
@@ -0,0 +1,34 @@
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
+import { useEffect } from "react";
+
+import MainNavigation from "../components/MainNavigation";
+import { getTokenDuration } from "../utils/auth";
+
+function RootLayout() {
+  // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) return;
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+    }
+    const tokenDuration = getTokenDuration();
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, tokenDuration);
+  }, [token, submit]);
+
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
